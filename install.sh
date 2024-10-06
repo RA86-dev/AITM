46cdd54f81abba7b7766263d5ff60cd6cbb70939
Ric <67565057+RA86-dev@users.noreply.github.com>
2024-10-06 12:06:43 -0500

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "======================================"
+echo "Artificial Intelligence Traffic Monitor"
+echo "======================================"
+git clone https://github.com/RA86-dev/AITM
+cd AITM 
+pip install -r requirements.txt
+echo "Done! Please run python3 index.py"
+wget https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/retinanet_resnet50_fpn_coco-eeacb38b.pth/
